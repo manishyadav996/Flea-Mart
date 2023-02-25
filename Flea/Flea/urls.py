b5b317e5f95db84e7f83c664d8817e985224bb55
Manish  Yadav <53707275+manishyadav996@users.noreply.github.com>
2023-02-25 17:11:42 +0530

    Update urls.py

--- Flea/Flea/urls.py
+++ Flea/Flea/urls.py
@@ -5,5 +5,7 @@ from main.views import index, contact
 
 urlpatterns = [
     path('', index, name='index'),
+    path('contact/', contact, name='contact'),
+    path('about/', about, name='about'),
     path('admin/', admin.site.urls),
 ]
